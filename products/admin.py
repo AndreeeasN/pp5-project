e1c28639c6b75bb0de67e246715d30d93c853b6c
Andreas Norberg Bergström <andorojas@gmail.com>
2023-08-24 22:26:45 +0200

    Customize admin menu

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,42 @@ from django.contrib import admin
 from .models import Product, Category, Brand, ProductTag
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(ProductTag)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'price',
+        'brand',
+        'image',
+    )
+    search_fields = ['sku', 'name', 'brand']
+    ordering = ('sku',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    ordering = ('friendly_name',)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'website',
+    )
+    ordering = ('name',)
+
+
+@admin.register(ProductTag)
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'search_visible',
+    )
+    ordering = ('name',)
