0fae9e50755d748a941e0c2d74e34b1b94352f5c
Andreas Norberg Bergström <andorojas@gmail.com>
2023-08-30 01:05:11 +0200

    Implement sort on products page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, ProductTag
 
 
@@ -12,25 +13,43 @@ def all_products(request):
     query = None
     category = None
     tags = None
+    sort = None
+    direction = None
 
-    # Filters based on category, tag and search query
     if request.GET:
+        # Sorting
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        # Category Filter
         if 'category' in request.GET:
             category = request.GET['category']
             products = products.filter(category__name=category)
             category = Category.objects.filter(name=category)
 
+        # Tag Filter
         if 'tags' in request.GET:
             tags = request.GET['tags'].split(',')
             products = products.filter(product_tags__name__in=tags).distinct
             tags = ProductTag.objects.filter(name__in=tags)
 
+        # Search Filter
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.warning(
                     request,
-                    "No search text entered, showing all products"
+                    "No search text entered."
                     )
                 return redirect(reverse('products'))
 
@@ -42,11 +61,14 @@ def all_products(request):
                 )
             products = products.filter(queries).distinct
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'current_category': category,
         'current_tags': tags,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
