6eefde5d620f67ed851789bafd0144d91cedcd59
Andreas Norberg Bergström <andorojas@gmail.com>
2023-09-04 03:28:25 +0200

    Add checkout views and models

--- pp5_django/settings.py
+++ pp5_django/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
 
     # other
     'storages',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,10 @@ TEMPLATES = [
                 # shopping cart contents
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
@@ -103,6 +109,9 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 WSGI_APPLICATION = 'pp5_django.wsgi.application'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Account Verification
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
